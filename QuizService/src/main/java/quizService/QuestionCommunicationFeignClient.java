66865d8054a5e758ef81330ff09e4b6c84d0ea5c
NayanChaturved <chaturvedinayan5>
2023-12-25 15:54:22 +0530

    all

--- QuizService/src/main/java/quizService/QuestionCommunicationFeignClient.java
+++ QuizService/src/main/java/quizService/QuestionCommunicationFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+@FeignClient(name = "QUESTION-SERVICE")
 public interface QuestionCommunicationFeignClient
 {
 	@GetMapping("/question/quiz/{quizId}")
